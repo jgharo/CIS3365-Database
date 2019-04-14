ced77a5419b46f3062cd38f2f62e91b874f2b106
rgarci53 <rgarcia51@uh.edu>
2019-04-14 13:45:35 -0500

    Rubi Update

--- src/Main/Information/UserManageController.java
+++ src/Main/Information/UserManageController.java
@@ -3,6 +3,8 @@ package Information;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -17,7 +19,8 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class UserManageController {
+public class UserManageController implements Initializable {
+    @FXML
     public TableView studentTable;
     public TableColumn col_stu_id;
     public TableColumn col_par_id;
@@ -32,7 +35,7 @@ public class UserManageController {
     public TableColumn col_stu_lang;
     public TableColumn col_stu_abs;
     public TableColumn col_date_mod;
-    
+    @FXML
     public Button deleteButton;
     public Button modifyButton;
     public Button addButton;
@@ -40,81 +43,59 @@ public class UserManageController {
     Scene returnScene;
 
 
+    ObservableList<informationStudent> students = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Connection conn=null;
+        Statement stmt=null;
+        String connectionURL=
+                "jdbc:sqlserver://localhost:1433;"
+                        +"database=test_2;"
+                        +"user=anson;"
+                        +"password=password;";
+        ObservableList<informationStudent> studentInfo = FXCollections.observableArrayList();
 
-        @FXML
-        private TableView<display_student_model> table;
-        @FXML
-        private TableColumn<display_student_model,String> col_stu_id;
-        @FXML
-        private TableColumn<display_student_model, String> col_par_id;
-        @FXML
-        private TableColumn<display_student_model, String> col_class_id;
-        @FXML
-        private TableColumn<display_student_model, String> col_course_id;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_fname;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_lname;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_dob;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_email;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_phone;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_address;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_lang;
-        @FXML
-        private TableColumn<display_student_model, String> col_stu_abs;
-        @FXML
-        private TableColumn<display_student_model, String> col_date_mod;
+        try {
 
-        ObservableList<display_student_model> oblist = FXCollections.observableArrayList();
+            Connection con = DBconnect.getConnection();
 
-        @Override
-        public void initialize(URL location, ResourceBundle resources){
+            CallableStatement stmtStu = con.prepareCall("{CALL student_Select()}");
 
-            try {
-
-                Connection con = DBconnection.getConnection();
-
-                CallableStatement stmtstu= con.prepareCall("{CALL student_select()}");
-
-                ResultSet rs= stmtstu.executeQuery();
-                while (rs.next()){
-                    oblist.add(new display_student_model(rs.getString("Stu_ID"),rs.getString("Par_ID"),rs.getString("Class_ID"),rs.getString("Course_ID"),
-                            rs.getString("Stu_FName"), rs.getString("Stu_LName"), rs.getString("Stu_DOB"),rs.getString("Stu_Email"),rs.getString("Stu_Phone"),
-                            rs.getString("Stu_Address"), rs.getString("Stu_Lang"), rs.getString("Stu_Abs"), rs.getString("Date_Mod")));
+            ResultSet rs = stmtStu.executeQuery();
+            while (rs.next()){
+                studentInfo.add(new informationStudent(rs.getString("Stu_ID"),rs.getString("Par_ID"),rs.getString("Class_ID"),rs.getString("Course_ID"),
+                        rs.getString("Stu_FName"), rs.getString("Stu_LName"), rs.getString("Stu_DOB"),rs.getString("Stu_Email"),rs.getString("Stu_Phone"),
+                        rs.getString("Stu_Address"), rs.getString("Stu_Lang"), rs.getString("Stu_Abs"), rs.getString("Date_Mod")));
 
 
-
-                }
-
-            } catch (SQLException ex){
-                Logger.getLogger(display_student_controller.class.getName()).log(Level.SEVERE, null, ex);
             }
 
+        } catch (SQLException ex) {
+            Logger.getLogger( InformationAdmin.class.getName()).log(Level.SEVERE, null, ex);
 
-            col_stu_id.setCellValueFactory(new PropertyValueFactory<>("Stu_ID"));
-            col_par_id.setCellValueFactory(new PropertyValueFactory<>("Par_ID"));
-            col_class_id.setCellValueFactory(new PropertyValueFactory<>("Class_ID"));
-            col_course_id.setCellValueFactory(new PropertyValueFactory<>("Course_ID"));
-            col_stu_fname.setCellValueFactory(new PropertyValueFactory<>("Stu_FName"));
-            col_stu_lname.setCellValueFactory(new PropertyValueFactory<>("Stu_LName"));
-            col_stu_dob.setCellValueFactory(new PropertyValueFactory<>("Stu_DOB"));
-            col_stu_email.setCellValueFactory(new PropertyValueFactory<>("Stu_Email"));
-            col_stu_phone.setCellValueFactory(new PropertyValueFactory<>("Stu_Phone"));
-            col_stu_address.setCellValueFactory(new PropertyValueFactory<>("Stu_Address"));
-            col_stu_lang.setCellValueFactory(new PropertyValueFactory<>("Stu_Lang"));
-            col_stu_abs.setCellValueFactory(new PropertyValueFactory<>("Stu_Abs"));
-            col_date_mod.setCellValueFactory(new PropertyValueFactory<>("Date_Mod"));
+        }
 
-            table.setItems(oblist);
+        col_stu_id.setCellValueFactory(new PropertyValueFactory<>("Stu_ID"));
+        col_par_id.setCellValueFactory(new PropertyValueFactory<>("Par_ID"));
+        col_class_id.setCellValueFactory(new PropertyValueFactory<>("Class_ID"));
+        col_course_id.setCellValueFactory(new PropertyValueFactory<>("Course_ID"));
+        col_stu_fname.setCellValueFactory(new PropertyValueFactory<>("Stu_FName"));
+        col_stu_lname.setCellValueFactory(new PropertyValueFactory<>("Stu_LName"));
+        col_stu_dob.setCellValueFactory(new PropertyValueFactory<>("Stu_DOB"));
+        col_stu_email.setCellValueFactory(new PropertyValueFactory<>("Stu_Email"));
+        col_stu_phone.setCellValueFactory(new PropertyValueFactory<>("Stu_Phone"));
+        col_stu_address.setCellValueFactory(new PropertyValueFactory<>("Stu_Address"));
+        col_stu_lang.setCellValueFactory(new PropertyValueFactory<>("Stu_Lang"));
+        col_stu_abs.setCellValueFactory(new PropertyValueFactory<>("Stu_Abs"));
+        col_date_mod.setCellValueFactory(new PropertyValueFactory<>("Date_Mod"));
+
+        studentTable.setItems(students);
+
 
     }
+
+
 
     public void deleteStudent(ActionEvent actionEvent) {
     }
