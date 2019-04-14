629d6deefa3c20e7f75dfb494c16b9145e3bf660
ahdao2 <ansondao1@gmail.com>
2019-04-13 22:35:07 -0500

    commit

---
+++ src/Main/report/repo_mccormick_5.java
@@ -0,0 +1,119 @@
+package report;
+
+
+public class repo_mccormick_5 {
+
+    String Par_ID, PReg_ID, Fath_FName, Fath_LName, Moth_FName, Moth_LName, Stu_ID, Stu_FName, Stu_LName, Trans_Remain, Due_Date, Par_Marital;
+
+    public repo_mccormick_5(String Par_ID, String PReg_ID, String Fath_FName, String Fath_LName, String Moth_FName, String Moth_LName, String Stu_ID, String Stu_FName, String Stu_LName, String Trans_Remain, String Due_Date, String Par_Marital) {
+        this.Par_ID = Par_ID;
+        this.PReg_ID = PReg_ID;
+        this.Fath_FName = Fath_FName;
+        this.Fath_LName = Fath_LName;
+        this.Moth_FName = Moth_FName;
+        this.Moth_LName = Moth_LName;
+        this.Stu_ID = Stu_ID;
+        this.Stu_FName = Stu_FName;
+        this.Stu_LName = Stu_LName;
+        this.Trans_Remain = Trans_Remain;
+        this.Due_Date = Due_Date;
+        this.Par_Marital = Par_Marital;
+
+    }
+
+    public String getPar_ID() {
+        return Par_ID;
+    }
+
+    public void setPar_ID(String par_ID) {
+        Par_ID = par_ID;
+    }
+
+    public String getPReg_ID() {
+        return PReg_ID;
+    }
+
+    public void setPReg_ID(String PReg_ID) {
+        this.PReg_ID = PReg_ID;
+    }
+
+    public String getFath_FName() {
+        return Fath_FName;
+    }
+
+    public void setFath_FName(String fath_FName) {
+        Fath_FName = fath_FName;
+    }
+
+    public String getFath_LName() {
+        return Fath_LName;
+    }
+
+    public void setFath_LName(String fath_LName) {
+        Fath_LName = fath_LName;
+    }
+
+    public String getMoth_FName() {
+        return Moth_FName;
+    }
+
+    public void setMoth_FName(String moth_FName) {
+        Moth_FName = moth_FName;
+    }
+
+    public String getMoth_LName() {
+        return Moth_LName;
+    }
+
+    public void setMoth_LName(String moth_LName) {
+        Moth_LName = moth_LName;
+    }
+
+    public String getStu_ID() {
+        return Stu_ID;
+    }
+
+    public void setStu_ID(String stu_ID) {
+        Stu_ID = stu_ID;
+    }
+
+    public String getStu_FName() {
+        return Stu_FName;
+    }
+
+    public void setStu_FName(String stu_FName) {
+        Stu_FName = stu_FName;
+    }
+
+    public String getStu_LName() {
+        return Stu_LName;
+    }
+
+    public void setStu_LName(String stu_LName) {
+        Stu_LName = stu_LName;
+    }
+
+    public String getTrans_Remain() {
+        return Trans_Remain;
+    }
+
+    public void setTrans_Remain(String trans_Remain) {
+        Trans_Remain = trans_Remain;
+    }
+
+    public String getDue_Date() {
+        return Due_Date;
+    }
+
+    public void setDue_Date(String due_Date) {
+        Due_Date = due_Date;
+    }
+
+    public String getPar_Marital() {
+        return Par_Marital;
+    }
+
+    public void setPar_Marital(String par_Marital) {
+        Par_Marital = par_Marital;
+    }
+}
