107437c2618ccaa4e0ebaef1648c98d23f202030
jgharo <jgharo@uh.edu>
2019-04-15 15:58:01 -0500

    Updated AdminRegistrationForm

--- src/Main/Information/AdminRegistrationFormController.java
+++ src/Main/Information/AdminRegistrationFormController.java
@@ -5,10 +5,16 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import javax.swing.*;
 import java.net.URL;
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -36,98 +42,152 @@ public class AdminRegistrationFormController implements Initializable {
     @FXML
     private TextField areg_date;
 
+    @FXML
+    public Button addButton;
+    public Button updateButton;
+    public Button deleteButton;
+    public Button menuButton;
 
-    ObservableList<AdminRegistrationForm> oblist = FXCollections.observableArrayList();
+    Scene returnScene;
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
+        ObservableList<AdminRegistrationForm> observableList = FXCollections.observableArrayList();
+        table.setItems(observableList);
+        table.getSelectionModel().selectedItemProperty().addListener((observable,oldval,newval) -> {
+            AdminRegistrationForm admin = newval;
+            if (admin != null) {
+                areg_id.setText(admin.getAReg_ID());
+                adm_id.setText(admin.getAdm_ID());
+                areg_date.setText(admin.getAReg_Date());}
+        });
+        table.getSelectionModel().clearSelection();
+
+        setAdminRegistrationFormTable();
+        displayDatabase();
+    }
+
+    private void setAdminRegistrationFormTable() {
+        col_areg_id.setCellValueFactory(new PropertyValueFactory<>("AReg_ID"));
+        col_adm_id.setCellValueFactory(new PropertyValueFactory<>("Adm_ID"));
+        col_areg_date.setCellValueFactory(new PropertyValueFactory<>("AReg_Date"));
+    }
+
+    private void displayDatabase() {
+        ObservableList<AdminRegistrationForm> adminInfo = FXCollections.observableArrayList();
 
         try {
 
             Connection con = DBconnect.getConnection();
 
-            CallableStatement stmt= con.prepareCall("{CALL admin_reg_select()}");
+            CallableStatement stmtAdmin = con.prepareCall("{CALL admin_reg_select()}");
 
-            ResultSet rs= stmt.executeQuery();
-            while (rs.next()){
-                oblist.add(new AdminRegistrationForm(rs.getString("AReg_ID"),rs.getString("Adm_ID"), rs.getString("AReg_Date")));
+            ResultSet rs = stmtAdmin.executeQuery();
+            while (rs.next()) {
+                adminInfo.add(new AdminRegistrationForm(rs.getString("AReg_ID"), rs.getString("adm_ID"), rs.getString("AReg_Date")));
 
             }
 
-        } catch (SQLException ex){
-            Logger.getLogger(AdminRegistrationFormController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminRegistrationForm.class.getName()).log(Level.SEVERE, null, ex);
+
         }
-
-        col_areg_id.setCellValueFactory(new PropertyValueFactory<>("AReg_ID"));
-        col_adm_id.setCellValueFactory(new PropertyValueFactory<>("Adm_ID"));
-        col_areg_date.setCellValueFactory(new PropertyValueFactory<>("AReg_Date"));
-
-        table.setItems(oblist);
-
+        table.setItems(adminInfo);
     }
 
-    @FXML
-    private void add_AdminRegistrationForm(ActionEvent event){
+    public void add_AdminRegistrationForm(ActionEvent event){
 
         try {
             Connection con = DBconnect.getConnection();
 
             CallableStatement stmt = con.prepareCall("{CALL admin_reg_insert(?,?)}");
 
-            stmt.setString(1, this.adm_id.getText());
-            stmt.setString(2, this.areg_date.getText());
+            stmt.setString(1, adm_id.getText());
+            stmt.setString(2, areg_date.getText());
+
             stmt.execute();
+            setAdminRegistrationFormTable();
+            displayDatabase();
+            table.getSelectionModel().clearSelection();
+            table.refresh();
             con.close();
 
         }
         catch (SQLException ex){
             Logger.getLogger(AdminRegistrationFormController.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            adm_id.setText("");
+            areg_date.setText("");
         }
 
     }
 
-    @FXML
-    private void delete_AdminRegistrationForm(ActionEvent event){
+    public void delete_AdminRegistrationForm(ActionEvent event){
 
         try {
             Connection con = DBconnect.getConnection();
 
             CallableStatement stmt = con.prepareCall("{CALL admin_reg_delete(?)}");
 
-            stmt.setString(1, this.areg_id.getText());
+            stmt.setString(1, areg_id.getText());
 
             stmt.execute();
+            setAdminRegistrationFormTable();
+            displayDatabase();
             con.close();
-
 
         }
         catch (SQLException ex){
             Logger.getLogger(AdminRegistrationFormController.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            areg_id.setText("");
         }
 
     }
 
-    @FXML
-    private void update_AdminRegistrationForm(ActionEvent event){
+    public void update_AdminRegistrationForm(ActionEvent event){
 
         try {
             Connection con = DBconnect.getConnection();
 
             CallableStatement stmt = con.prepareCall("{CALL admin_reg_update(?,?,?)}");
 
-            stmt.setString(1, this.areg_id.getText());
-            stmt.setString(2, this.adm_id.getText());
-            stmt.setString(3, this.areg_date.getText());
+            stmt.setString(1, areg_id.getText());
+            stmt.setString(2, adm_id.getText());
+            stmt.setString(3, areg_date.getText());
 
-            stmt.execute();
+            stmt.executeUpdate();
+            setAdminRegistrationFormTable();
+            displayDatabase();
+            table.getSelectionModel().clearSelection();
+            table.refresh();
             con.close();
-
 
         }
         catch (SQLException ex){
             Logger.getLogger(AdminRegistrationFormController.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            areg_id.setText("");
+            adm_id.setText("");
+            areg_date.setText("");
         }
 
+    }
+
+    public void mainmenu(ActionEvent actionEvent) {
+        var stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(returnScene);
+
+    }
+
+    public void clear_AdminRegistrationForm(ActionEvent actionEvent) {
+        areg_id.setText("");
+        adm_id.setText("");
+        areg_date.setText("");
     }
 
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Information/AdminAccount.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Information/AdminRegistrationForm.fxml"));
         primaryStage.setTitle("St. Philip of Jesus Management System");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
