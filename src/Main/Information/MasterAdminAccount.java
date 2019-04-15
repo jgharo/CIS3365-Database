d8d2971382c545542bd9d2808bc880f5c3aa8642
ahdao2 <ansondao1@gmail.com>
2019-04-14 22:30:57 -0500

    Updated Master Admin Account

--- src/Main/Information/MasterAdminAccount.java
+++ src/Main/Information/MasterAdminAccount.java
@@ -1,14 +1,15 @@
 package Information;
 
-public class Master_Admin_Account {
+public class MasterAdminAccount {
 
-    String MAdm_Acc_ID, MAdm_ID, MAdm_User, MAdm_Pass;
+    String MAdm_Acc_ID,MAdm_ID, MAdm_User, MAdm_Pass;
 
-    public Master_Admin_Account(String MAdm_Acc_ID, String MAdm_ID, String MAdm_User, String MAdm_Pass) {
-        this.MAdm_Acc_ID = MAdm_Acc_ID;
-        this.MAdm_ID = MAdm_ID;
-        this.MAdm_User = MAdm_User;
-        this.MAdm_Pass = MAdm_Pass;
+    public MasterAdminAccount(String MAdm_Acc_ID, String MAdm_ID, String MAdm_User, String MAdm_Pass){
+        this.MAdm_Acc_ID=MAdm_Acc_ID;
+        this.MAdm_ID=MAdm_ID;
+        this.MAdm_User=MAdm_User;
+        this.MAdm_Pass=MAdm_Pass;
+
     }
 
     public String getMAdm_Acc_ID() {
---
+++ src/Main/Information/MasterAdminAccountController.java
@@ -0,0 +1,140 @@
+package Information;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MasterAdminAccountController implements Initializable {
+
+    @FXML
+    private TableView<MasterAdminAccount> table;
+    @FXML
+    private TableColumn<MasterAdminAccount,String> col_madm_acc_id;
+    @FXML
+    private TableColumn<MasterAdminAccount, String> col_madm_id;
+    @FXML
+    private TableColumn<MasterAdminAccount, String> col_madm_user;
+    @FXML
+    private TableColumn<MasterAdminAccount, String> col_madm_pass;
+
+    @FXML
+    private TextField madm_acc_id;
+    @FXML
+    private TextField madm_id;
+    @FXML
+    private TextField madm_user;
+    @FXML
+    private TextField madm_pass;
+
+
+    ObservableList<MasterAdminAccount> oblist = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+        try {
+
+            Connection con = DBconnect.getConnection();
+
+            CallableStatement stmt= con.prepareCall("{CALL master_admin_account_select()}");
+
+            ResultSet rs= stmt.executeQuery();
+            while (rs.next()){
+                oblist.add(new MasterAdminAccount(rs.getString("Madm_Acc_ID"),rs.getString("Madm_ID"), rs.getString("Madm_User"), rs.getString("Madm_Pass")));
+
+            }
+
+        } catch (SQLException ex){
+            Logger.getLogger(MasterAdminAccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        col_madm_acc_id.setCellValueFactory(new PropertyValueFactory<>("Madm_Acc_ID"));
+        col_madm_id.setCellValueFactory(new PropertyValueFactory<>("Madm_ID"));
+        col_madm_user.setCellValueFactory(new PropertyValueFactory<>("Madm_User"));
+        col_madm_pass.setCellValueFactory(new PropertyValueFactory<>("Madm_Pass"));
+
+        table.setItems(oblist);
+
+    }
+
+    @FXML
+    private void add_MasterAdminAccount(ActionEvent event){
+
+        try {
+            Connection con = DBconnect.getConnection();
+
+            CallableStatement stmt = con.prepareCall("{CALL master_admin_account_insert(?,?,?)}");
+
+            stmt.setString(1, this.madm_id.getText());
+            stmt.setString(2, this.madm_user.getText());
+            stmt.setString(3, this.madm_pass.getText());
+            stmt.execute();
+            con.close();
+
+        }
+        catch (SQLException ex){
+            Logger.getLogger(MasterAdminAccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    private void delete_MasterAdminAccount(ActionEvent event){
+
+        try {
+            Connection con = DBconnect.getConnection();
+
+            CallableStatement stmt = con.prepareCall("{CALL master_admin_account_delete(?)}");
+
+            stmt.setString(1, this.madm_acc_id.getText());
+
+            stmt.execute();
+            con.close();
+
+
+        }
+        catch (SQLException ex){
+            Logger.getLogger(MasterAdminAccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    private void update_MasterAdminAccount(ActionEvent event){
+
+        try {
+            Connection con = DBconnect.getConnection();
+
+            CallableStatement stmt = con.prepareCall("{CALL master_admin_account_update(?,?,?,?)}");
+
+            stmt.setString(1, this.madm_acc_id.getText());
+            stmt.setString(2, this.madm_id.getText());
+            stmt.setString(3, this.madm_user.getText());
+            stmt.setString(4, this.madm_pass.getText());
+
+            stmt.execute();
+            con.close();
+
+
+        }
+        catch (SQLException ex){
+            Logger.getLogger(MasterAdminAccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
