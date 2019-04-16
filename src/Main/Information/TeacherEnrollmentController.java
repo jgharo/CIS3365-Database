85d3d99574f2ee0775f86a675a068e45f2af0177
ahdao2 <ansondao1@gmail.com>
2019-04-16 14:04:56 -0500

    Updated Teacher Enrollment

--- src/Main/Information/TeacherEnrollmentController.java
+++ src/Main/Information/TeacherEnrollmentController.java
@@ -30,7 +30,7 @@ public class TeacherEnrollmentController implements Initializable {
     @FXML
     private TableView<TeacherEnrollment> table;
     @FXML
-    private TableColumn<TeacherEnrollment,String> col_teach_id;
+    private TableColumn<TeacherEnrollment, String> col_teach_id;
     @FXML
     private TableColumn<TeacherEnrollment, String> col_course_id;
     @FXML
@@ -44,10 +44,38 @@ public class TeacherEnrollmentController implements Initializable {
     private TextField teach_enroll;
 
 
-    ObservableList<TeacherEnrollment> oblist = FXCollections.observableArrayList();
-
+    Scene returnScene;
     @Override
-    public void initialize(URL location, ResourceBundle resources){
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<TeacherEnrollment> observableList = FXCollections.observableArrayList();
+        table.setItems(observableList);
+        table.getSelectionModel().selectedItemProperty().addListener((observable,oldval,newval) -> {
+            TeacherEnrollment stu = newval;
+            if (stu != null) {
+                teach_id.setText(stu.getTeach_ID());
+                course_id.setText(stu.getCourse_ID());
+                teach_enroll.setText(stu.getTeach_Enroll());
+
+
+            }
+        });
+        table.getSelectionModel().clearSelection();
+
+        setTeacherEnrTable();
+        displayDatabase();
+
+    }
+
+    private void setTeacherEnrTable() {
+        col_teach_id.setCellValueFactory(new PropertyValueFactory<>("Teach_ID"));
+        col_course_id.setCellValueFactory(new PropertyValueFactory<>("Course_ID"));
+        col_teach_enroll.setCellValueFactory(new PropertyValueFactory<>("Teach_Enroll"));
+    }
+
+
+    private void displayDatabase(){
+
+        ObservableList<TeacherEnrollment> teachenrInfo = FXCollections.observableArrayList();
 
         try {
 
@@ -57,7 +85,7 @@ public class TeacherEnrollmentController implements Initializable {
 
             ResultSet rs= stmt.executeQuery();
             while (rs.next()){
-                oblist.add(new TeacherEnrollment(rs.getString("Teach_ID"),rs.getString("Course_ID"), rs.getString("Teach_Enroll")));
+                teachenrInfo.add(new TeacherEnrollment(rs.getString("Teach_ID"),rs.getString("Course_ID"), rs.getString("Teach_Enroll")));
 
             }
 
@@ -65,11 +93,7 @@ public class TeacherEnrollmentController implements Initializable {
             Logger.getLogger(TeacherEnrollmentController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        col_teach_id.setCellValueFactory(new PropertyValueFactory<>("Teach_ID"));
-        col_course_id.setCellValueFactory(new PropertyValueFactory<>("Course_ID"));
-        col_teach_enroll.setCellValueFactory(new PropertyValueFactory<>("Teach_Enroll"));
-
-        table.setItems(oblist);
+        table.setItems(teachenrInfo);
 
     }
 
@@ -85,11 +109,20 @@ public class TeacherEnrollmentController implements Initializable {
             stmt.setString(2, this.course_id.getText());
             stmt.setString(3, this.teach_enroll.getText());
             stmt.execute();
+            setTeacherEnrTable();
+            displayDatabase();
+            table.getSelectionModel().clearSelection();
+            table.refresh();
             con.close();
 
         }
         catch (SQLException ex){
             Logger.getLogger(TeacherEnrollmentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            teach_id.setText("");
+            course_id.setText("");
+            teach_enroll.setText("");
         }
 
     }
@@ -105,12 +138,19 @@ public class TeacherEnrollmentController implements Initializable {
             stmtstu.setString(1, this.teach_id.getText());
 
             stmtstu.execute();
+            setTeacherEnrTable();
+            displayDatabase();
             con.close();
 
 
         }
         catch (SQLException ex){
             Logger.getLogger(TeacherEnrollmentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            teach_id.setText("");
+           course_id.setText("");
+            teach_enroll.setText("");
         }
 
     }
@@ -128,6 +168,10 @@ public class TeacherEnrollmentController implements Initializable {
             stmt.setString(3, this.teach_enroll.getText());
 
             stmt.execute();
+            setTeacherEnrTable();
+            displayDatabase();
+            table.getSelectionModel().clearSelection();
+            table.refresh();
             con.close();
 
 
@@ -136,8 +180,20 @@ public class TeacherEnrollmentController implements Initializable {
             Logger.getLogger(TeacherEnrollmentController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        finally{
+            teach_id.setText("");
+            course_id.setText("");
+            teach_enroll.setText("");
+        }
+
     }
 
+    @FXML
+    private void clear_TeacherEnrollment(ActionEvent actionEvent) {
+        teach_id.setText("");
+        course_id.setText("");
+        teach_enroll.setText("");
+    }
 
     @SuppressWarnings("Duplicates")
     public void backButtonPushed(ActionEvent event) throws IOException
@@ -148,4 +204,5 @@ public class TeacherEnrollmentController implements Initializable {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
 }
