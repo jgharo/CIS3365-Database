e28a9aac868a8d877c7a292744c3fa9dbeba13da
jgharo <jgharo@uh.edu>
2019-04-17 02:14:56 -0500

    updated libraries and artifacts

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login Screen.fxml"));
